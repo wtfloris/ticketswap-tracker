e37b8f24cb02d72f9005eaa2278c288421e23dea
Floris Heringa <mail@florisheringa.nl>
2018-02-18 15:13:42 +0100

    Initial push

---
+++ tswp_lib.py
@@ -0,0 +1,58 @@
+import requests
+import json
+from bs4 import BeautifulSoup
+import datetime as dt
+import time
+from os import rename
+
+#event_url = "imagine-dragons-evolve-world-tour/floor/c935b272-3019-48b9-877e-77f7f95b9a61/510954"
+#url = "https://www.ticketswap.nl/event/"+event_url
+
+def get_event_stats(url):
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, 'lxml')
+    available = -1
+    sold = -1
+    wanted = -1
+    try:
+        available = soup.find("div", {'class' : 'counter counter-available'}).find("div", {'class' : 'counter-value'}).text
+        sold = soup.find("div", {'class' : 'counter counter-sold'}).find("div", {'class' : 'counter-value'}).text
+        wanted = soup.find("div", {'class' : 'counter counter-wanted'}).find("div", {'class' : 'counter-value'}).text
+    except:
+        name = url[31:]
+        name = name[:name.index("/")].split("-")
+        print(name)
+    return [int(available), int(sold), int(wanted)]
+
+def get_event_title(url):
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, 'lxml')
+    
+    return soup.find("h1", {'itemprop' : 'summary'}).find("a").text
+    
+def get_event_date(url):
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, 'lxml')
+    
+    monthdict = {'januari' : '1', 'februari' : '2', 'maart' : '3', 'april' : '4', 'mei' : '5', 'juni' : '6', 'juli' : '7', 'augustus' : '8', 'september' : '9', 'oktober' : '10', 'november' : '11', 'december' : '12',}
+
+    date = soup.find("span", {'class' : 'date'}).text[1:]
+    date = date[date.index(" ")+1:]
+    day = date[:date.index(" ")]
+    month = monthdict[date[date.index(" ")+1:-5]]
+    year = date[-4:]
+
+    event_date = dt.datetime(int(year), int(month), int(day), 21)
+    t = dt.datetime.today()
+    today = dt.datetime(t.year, t.month, t.day, t.hour)
+
+    event_timestamp = time.mktime(event_date.timetuple())
+    return event_timestamp
+
+def get_date_diff(event_timestamp):
+    today_timestamp = int(time.time())
+
+    diff_s = event_timestamp - today_timestamp
+    diff_h = int(diff_s/3600.0)
+
+    return diff_h
---
+++ tswp_script.py
@@ -0,0 +1,30 @@
+import tswp_lib as tswp
+from os import rename
+
+r = open("daily", "r")
+w = open("daily_tmp", "w")
+results = []
+for line in r.readlines():
+    print(line)
+    if ':' in line:
+        w.write(line)
+        url = "https://www.ticketswap.nl/event/"+line[:line.index(":")]
+        print(int(line[line.index(":")+1:-1]))
+        diff = tswp.get_date_diff(int(line[line.index(":")+1:-1]))
+        if diff > 0:
+            with open("stats_"+line[:line.index("/")], 'a') as f:
+                stats = [diff]
+                stats = stats+tswp.get_event_stats(url)
+                f.write(str(stats)+'\n')
+                print(stats)
+        else:
+            with open("ERROR_"+line[:line.index("/")], 'a') as f:
+                f.write('error')
+    else:
+        url = "https://www.ticketswap.nl/event/"+line
+        event_timestamp = tswp.get_event_date(url)
+        print(event_timestamp)
+        w.write(line[:-1]+":"+str(int(event_timestamp))+'\n')
+r.close()
+w.close()
+rename("daily_tmp", "daily")
