7e98dc4b725a0d59d21febae405aedcdaa1da931
Floris Heringa <mail@florisheringa.nl>
2018-09-20 15:20:07 +0200

    bugfix probably

--- tswp_lib.py
+++ tswp_lib.py
@@ -37,6 +37,12 @@ def get_event_date(url):
     monthdict = {'januari' : '1', 'februari' : '2', 'maart' : '3', 'april' : '4', 'mei' : '5', 'juni' : '6', 'juli' : '7', 'augustus' : '8', 'september' : '9', 'oktober' : '10', 'november' : '11', 'december' : '12',}
 
     date = soup.find("span", {'class' : 'date'}).text[1:]
+    
+try:
+    date = date[:date.index("-")-1] 
+except:
+    pass
+    
     date = date[date.index(" ")+1:]
     day = date[:date.index(" ")]
     month = monthdict[date[date.index(" ")+1:-5]]
