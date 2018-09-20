782a8034bd1ada0deaa1c60f2e5864116cde3ca9
Floris Heringa <mail@florisheringa.nl>
2018-09-20 15:37:17 +0200

    bugfix probably

--- tswp_script.py
+++ tswp_script.py
@@ -2,12 +2,14 @@ import tswp_lib as tswp
 from os import rename
 import sys
 
+path = "/etc/tswp_metric/"
+
 if sys.argv[1] == "daily":
-    r = open("daily", "r")
-    w = open("daily_tmp", "w")
+    r = open(path+"daily", "r")
+    w = open(path+"daily_tmp", "w")
 if sys.argv[1] == "hourly":
-    r = open("hourly", "r")
-    w = open("hourly_tmp", "w")
+    r = open(path+"hourly", "r")
+    w = open(path+"hourly_tmp", "w")
 
 results = []
 for line in r.readlines():
@@ -17,7 +19,7 @@ for line in r.readlines():
         print(int(line[line.index(":")+1:-1]))
         diff = tswp.get_date_diff(int(line[line.index(":")+1:-1]))
         if diff < 48 and sys.argv[1] == "daily":
-            with open("hourly", "w") as hourly_wr:
+            with open(path+"hourly", "w") as hourly_wr:
                 hourly_wr.write(line)
                 print("Event in less than 48 hours, moving to hourly")
         if diff > 0:
@@ -25,14 +27,14 @@ for line in r.readlines():
                 w.write(line)
             if diff < 48 and sys.argv[1] == "hourly":
                 w.write(line)
-            with open("stats_"+line[:line.index("/")], 'a') as f:
+            with open(path+"stats_"+line[:line.index("/")], 'a') as f:
                 print(str(diff)+" hours left until the event starts")
                 stats = [diff]
                 stats = stats+tswp.get_event_stats(url)
                 f.write(str(stats)+'\n')
                 print(stats)
         else:
-            with open("ERROR_"+line[:line.index("/")], 'a') as f:
+            with open(path+"ERROR_"+line[:line.index("/")], 'a') as f:
                 f.write('error')
     else:
         url = "https://www.ticketswap.nl/event/"+line[:-1]
@@ -41,7 +43,7 @@ for line in r.readlines():
 r.close()
 w.close()
 if sys.argv[1] == "daily":
-    rename("daily_tmp", "daily")
+    rename(path+"daily_tmp", path+"daily")
 if sys.argv[1] == "hourly":
-    rename("hourly_tmp", "hourly")
+    rename(path+"hourly_tmp", path+"hourly")
 
